d71a1a07b820b1ecb6b850f2fa440725d75d6252
M <malekuk9@gmail.com>
2025-06-01 18:30:12 +0100

    feat hook to fire on detected word

--- frontend/utils/hooks/Listen.js
+++ frontend/utils/hooks/Listen.js
@@ -2,7 +2,7 @@ import {
   ExpoSpeechRecognitionModule,
   useSpeechRecognitionEvent,
 } from "expo-speech-recognition";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function stop() {
   ExpoSpeechRecognitionModule.stop();
@@ -21,6 +21,9 @@ async function start() {
     lang: "en-US",
     interimResults: true,
     continuous: true,
+    androidIntentOptions: { EXTRA_LANGUAGE_MODEL: "web_search" },
+    iosTaskHint: "confirmation",
+    requiresOnDeviceRecognition: true,
   });
 }
 
@@ -28,12 +31,31 @@ export default function useListen() {
   const [transcript, setTranscript] = useState("");
 
   useSpeechRecognitionEvent("result", (event) => {
-    console.log("start");
+    console.log("result:", event.results);
     setTranscript(event.results[0]?.transcript);
+  });
+  useSpeechRecognitionEvent("end", (event) => {
+    setTranscript("");
   });
   useSpeechRecognitionEvent("error", (event) => {
     console.log("error code:", event.error, "error message:", event.message);
   });
 
   return [transcript, start, stop];
+}
+
+export function useDetect(wordToDetect, onDetect) {
+  const [transcript] = useListen();
+  const [words, setWords] = useState([]);
+  useEffect(() => {
+    setWords(transcript.split(" ").map((word) => word.toLowerCase()));
+  }, [transcript]);
+  const detected = words.filter((word) => word === wordToDetect.toLowerCase());
+  if (detected.length) {
+    onDetect();
+    stop();
+    setWords([]);
+    start();
+  }
+  return [start, stop];
 }
