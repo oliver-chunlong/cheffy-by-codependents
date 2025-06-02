28257c322aa1bec9ea4ef57f0bfc65525e8d054e
Sara-Lib <jigoujitoku@gmail.com>
2025-06-02 09:48:10 +0100

    Seeded the database

---
+++ backend/db/connection.js
@@ -0,0 +1,22 @@
+const { Pool } = require('pg');
+
+const ENV = process.env.NODE_ENV || 'development';
+
+require('dotenv').config({ path: `${__dirname}/../.env.${ENV}` });
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error('No PGDATABASE or DATABASE_URL configured');
+} else {
+  console.log(`Connected to ${process.env.PGDATABASE || process.env.DATABASE_URL}`);
+}
+
+const config = {};
+
+if (ENV === 'production') {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+} else {
+  config.database = process.env.PGDATABASE; // for local/dev
+}
+
+module.exports = new Pool(config);
--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -3,25 +3,6 @@ CREATE TABLE users (
   username TEXT UNIQUE NOT NULL
 );
 
---separated into different tables and connected them
-CREATE TABLE user_dietary_restrictions (
-  user_id INTEGER REFERENCES users(user_id),
-  restriction_id INTEGER REFERENCES dietary_restrictions(restriction_id),
-  PRIMARY KEY (user_id, restriction_id)
-);
-
-CREATE TABLE user_cooked_recipes (
-  user_id INTEGER REFERENCES users(user_id),
-  recipe_id INTEGER REFERENCES recipes(recipe_id),
-  PRIMARY KEY (user_id, recipe_id)
-);
-
-CREATE TABLE user_favorites (
-  user_id INTEGER REFERENCES users(user_id),
-  recipe_id INTEGER REFERENCES recipes(recipe_id),
-  PRIMARY KEY (user_id, recipe_id)
-);
-
 CREATE TABLE recipes (
   recipe_id SERIAL PRIMARY KEY,
   recipe_name TEXT NOT NULL,
@@ -32,7 +13,7 @@ CREATE TABLE recipes (
 
 CREATE TABLE ingredients (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
+  ingredient_name VARCHAR(255) NOT NULL,
   img_url TEXT,
   calories_per_100g INT,
   protein_g_per_100g FLOAT,
@@ -47,19 +28,31 @@ CREATE TABLE dietary_restrictions (
   restriction_name TEXT UNIQUE NOT NULL
 );
 
-CREATE TABLE ingredient_dietary_restrictions (
-  ingredient_id INTEGER REFERENCES ingredients(ingredient_id),
+CREATE TABLE user_dietary_restrictions (
+  user_id INTEGER REFERENCES users(user_id),
   restriction_id INTEGER REFERENCES dietary_restrictions(restriction_id),
-  PRIMARY KEY (ingredient_id, restriction_id)
+  PRIMARY KEY (user_id, restriction_id)
 );
 
-CREATE TABLE ingredient_quantities (
-  iq_id SERIAL PRIMARY KEY,
+
+CREATE TABLE user_cooked_recipes (
+  user_id INTEGER REFERENCES users(user_id),
   recipe_id INTEGER REFERENCES recipes(recipe_id),
-  ingredient_id INTEGER REFERENCES ingredients(ingredient_id),
-  quantity_numerical NUMERIC,
-  quantity_unit TEXT,
-  optional BOOLEAN DEFAULT false
+  PRIMARY KEY (user_id, recipe_id)
+);
+
+
+CREATE TABLE user_favorites (
+  user_id INTEGER REFERENCES users(user_id),
+  recipe_id INTEGER REFERENCES recipes(recipe_id),
+  PRIMARY KEY (user_id, recipe_id)
+);
+
+
+CREATE TABLE ingredient_dietary_restrictions (
+  ingredient_id INTEGER REFERENCES ingredients(id),
+  restriction_id INTEGER REFERENCES dietary_restrictions(restriction_id),
+  PRIMARY KEY (ingredient_id, restriction_id)
 );
 
 CREATE TABLE equipment (
@@ -68,11 +61,22 @@ CREATE TABLE equipment (
   equipment_img_url TEXT
 );
 
+CREATE TABLE ingredient_quantities (
+  iq_id SERIAL PRIMARY KEY,
+  recipe_id INTEGER REFERENCES recipes(recipe_id),
+  ingredient_id INTEGER REFERENCES ingredients(id),
+  quantity_numerical NUMERIC,
+  quantity_unit TEXT,
+  optional BOOLEAN DEFAULT false
+);
+
+
 CREATE TABLE instructions (
   instruction_id SERIAL PRIMARY KEY,
   recipe_id INTEGER REFERENCES recipes(recipe_id),
   step_number INTEGER NOT NULL,
   step_description TEXT NOT NULL,
-  time_required INTEGER,
+  iq_id INTEGER REFERENCES ingredient_quantities(iq_id),
+  time_required NUMERIC,
   timed_task BOOLEAN DEFAULT false
 );
