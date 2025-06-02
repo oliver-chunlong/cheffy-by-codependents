a707a39f7d08ab7b6148b2d5d377c8e59613f7a6
M <malekuk9@gmail.com>
2025-06-02 17:13:47 +0100

    feat recipe details

--- frontend/components/HomePage-components/RecipeCard.jsx
+++ frontend/components/HomePage-components/RecipeCard.jsx
@@ -1,12 +1,16 @@
 import { View } from "react-native";
 import { GridListItem } from "react-native-ui-lib";
 
+import { useNavigation } from "@react-navigation/native";
+
 export default function RecipeCard({ recipe }) {
+  const navigation = useNavigation();
   return (
     <GridListItem
       title={recipe.recipe_name}
       description={recipe.recipe_description}
       imageProps={{ source: { uri: recipe.recipe_img_url } }}
+      onPress={() => navigation.navigate("RecipeDetail")}
     />
   );
 }
