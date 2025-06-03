c372e334496c7122b5d9e09f1dd7a869dc1fa2e4
Nick <nj.ratnarajah@gmail.com>
2025-06-03 12:29:17 +0100

    feat: add utils for axios db query in recipeList component

---
+++ frontend/utils/axios.jsx
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const endpoint = "SUPABASE_URL_GOES_HERE";
+
+export const requestRecipes = (searchQuery, filterBy, category, order) => {
+    const params = new URLSearchParams ();
+
+    if (searchQuery)
+        params.append("search", searchQuery)
+    if (filterBy)
+        params.append("filter", filterBy)
+    if (category)
+        params.append("category", category)
+    if (order)
+        params.append("order", order)
+
+    return axios.get(`${endpoint}/recipes?${params.toString()}`)
+        .then((response) => {
+            return response.data.recipes
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+}
