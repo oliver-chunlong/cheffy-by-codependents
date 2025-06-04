388f06aa5d3dcc85c9247c849940df9558765bbb
M <malekuk9@gmail.com>
2025-06-04 11:35:55 +0100

    feat recipes connect to db

--- frontend/components/HomePage-components/RecipeList.jsx
+++ frontend/components/HomePage-components/RecipeList.jsx
@@ -7,20 +7,11 @@ import { requestRecipes } from "../../utils/axios";
 export default function RecipeList({ searchQuery, filterBy, category }) {
   //changes to filtering logic required
 
-  const [allRecipes, setAllRecipes] = useState(
-    [1, 2, 3, 4, 5, 6, 7, 8, 9].map((index) => ({
-      id: index,
-      recipe_name: "Pasta",
-      recipe_description: "Good",
-      recipe_img_url:
-        "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.pixabay.com%2Fphoto%2F2016%2F04%2F15%2F09%2F10%2Fpasta-1330541_960_720.jpg&f=1&nofb=1&ipt=9bf6aeddefd9e7c4737bd557fe3ea88371137a67224cdd7df784e6a0dd6814e4",
-    }))
-  );
+  const [allRecipes, setAllRecipes] = useState([]);
 
-  useEffect(() => {
-    // requestRecipes.then((recipes) => {
-    //   setAllRecipes(recipes);
-    // });
+  useEffect(async () => {
+    const recipes = await requestRecipes();
+    setAllRecipes(recipes);
     // useEffect(() => {
     //   /*Function to get recipes from the database*/
     //   /*UtilFunctionHere*/ .then((recipes) => {
@@ -28,7 +19,7 @@ export default function RecipeList({ searchQuery, filterBy, category }) {
     //     setFilteredRecipes(recipes);
     //   });
     // }, []);
-  });
+  }, []);
 
   return (
     <GridList
