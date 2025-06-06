e29ae65d41d9ce3af7b3ec19e56845601807d9c5
Bryxny <bryonydoh2@gmail.com>
2025-06-06 15:21:47 +0100

    feat: favourite component

---
+++ frontend/components/FavouriteButton.jsx
@@ -0,0 +1,72 @@
+import { Button, View } from "react-native-ui-lib";
+import {
+  postRecipeToFavourites,
+  requestFavouriteRecipes,
+  removeRecipeFromFavourites,
+} from "../utils/axios";
+import { useContext, useEffect, useState } from "react";
+import { UserContext } from "../context/UserContext.jsx";
+import Heart from "../assets/Heart.png";
+import HeartClicked from "../assets/HeartClicked.png";
+import Toast from "react-native-toast-message";
+
+export default function FavouriteButton({ recipe_id }) {
+  const { user } = useContext(UserContext);
+  const [isClicked, setIsClicked] = useState(false);
+
+  useEffect(() => {
+    if (!user) return;
+
+    requestFavouriteRecipes(user.id).then((res) => {
+      const favourites = res.map((recipe) => recipe.recipe_id);
+      if (favourites.includes(recipe_id)) {
+        setIsClicked(true);
+      }
+    });
+  }, [user, recipe_id]);
+
+  const handleToggle = () => {
+    if (!user) {
+      Toast.show({
+        type: "error",
+        text1: "Please log in to favourite",
+      });
+      return;
+    }
+    const newClickedState = !isClicked;
+    setIsClicked(newClickedState);
+
+    const action = newClickedState
+      ? postRecipeToFavourites(user.id, recipe_id)
+      : removeRecipeFromFavourites(user.id, recipe_id);
+
+    action
+      .then(() => {
+        Toast.show({
+          type: "success",
+          text1: newClickedState
+            ? "Added to favourites"
+            : "Removed from favourites",
+        });
+      })
+      .catch(() => {
+        setIsClicked(!newClickedState);
+        Toast.show({
+          type: "error",
+          text1: "Something went wrong...",
+        });
+      });
+  };
+
+  return (
+    <View>
+      <Button
+        title="Add to favourites"
+        onPress={handleToggle}
+        iconSource={isClicked ? HeartClicked : Heart}
+        iconStyle={{ width: 30, height: 30, tintColor: undefined }}
+        backgroundColor="transparent"
+      />
+    </View>
+  );
+}
