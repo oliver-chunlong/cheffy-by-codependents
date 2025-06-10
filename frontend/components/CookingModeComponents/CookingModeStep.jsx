cf4a994eb10c62d5d60a7d50bfe0c073c8e47343
Bryxny <bryonydoh2@gmail.com>
2025-06-10 09:26:56 +0100

    fix: speech recog crashing

--- frontend/components/CookingModeComponents/CookingModeStep.jsx
+++ frontend/components/CookingModeComponents/CookingModeStep.jsx
@@ -76,12 +76,3 @@ export default function CookingModeStep({
     </Card>
   );
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: "center",
-//     backgroundColor: "#ecf0f1",
-//     padding: 8,
-//   },
-// });
